f9026ec95d1dd9d1f9958d4748e18aad4af012fb
wenzodev <daniel.wenczuk@gmail.com>
2022-08-10 15:10:05 +0200

    updated version for mobile and tablets

--- js/data_testimonials.js
+++ js/data_testimonials.js
@@ -2,45 +2,46 @@
 
 const people = [
 {
+    text: `"Wojtek Dyjak to profesjonalista. Bezbłędnie znajduje miejsca bólu i pośwęca im należytą uwagę. Jego masaż to nie tylko wielka ulga dla ciała, ale także przyjemnie spędzony czas. Wojtek to fantastyczny rozmówca, pełen dyskrecji, który świetnie wyczuwa potrzeby i nastroje klientów".`,
     name: "Agata Trzebuchowska",
     job: "aktorka, reżyserka, scenarzystka",
-    text: `"Wojtek Dyjak to profesjonalista. Bezbłędnie znajduje miejsca bólu i pośwęca im należytą uwagę. Jego masaż to nie tylko wielka ulga dla ciała, ale także przyjemnie spędzony czas. Wojtek to fantastyczny rozmówca, pełen dyskrecji, który świetnie wyczuwa potrzeby i nastroje klientów".`,
     number: "1/7",
 },
 {
+    text: `Pan Wojciech Dyjak jest moim masażystą od 11 lat. Spotykamy się regularnie raz w tygodniu. Pan Wojtek jest przede wszystkim bardzo sumienny w wykonywaniu swojej pracy. Masaże są bardzo urozmaicone, wykonywane różnymi technikami, co powoduje, że nigdy się nie nudzą. Ja preferuję masaż mocny, i tu Pan Wojtek nie ma knkurencji! Oczywście o wyborze masażu: silny czy relaksacyjny decyduje klient. Ważną rzeczą jest, że jeśli Pan Wojtek dysponuje czasem, często masaż wydłuża się. Nie należy on do tych masażystów, którzy z zegarkiem w ręku trzymają się swoich 60 minut, ani sekundy dłużej! Ja bardzo polecam!.`,
     name: "Marzena Buczyńska",
     job: "aktorka",
-    text: `Pan Wojciech Dyjak jest moim masażystą od 11 lat. Spotykamy się regularnie raz w tygodniu. Pan Wojtej jest przede wszystkim bardzo sumienny w wykonywaniu swojej pracy. Masaże są bardzo urozmaicone, wykonywane różnymi technikami, co powoduje, że nigdy się nie nudzą. Ja preferuję masaż mocny, i tu Pan Wojtek nie ma knkurencji! Oczywście o wyborze masażu: silny czy relaksacyjny decyduje klient. Ważną rzeczą jest, że jeśli Pan Wojtek dysponuje czasem, często masaż wydłuża się. Nie należy on do tych masażystów, którzy z zegarkiem w ręku trzymają się swoich 60 minut, ani sekundy dłużej! Ja bardzo polecam!.`,
     number: "2/7",
 },
 {
+    text: `"W każdym zawodzie istnieją rzemieślnicy i artyści. Ci pierwsi nas zadowalają, ci drudzy wywołują zachwyt. Pan Wojtek Dyjak jest artystą! Ma talent, wiedzę i pasję do wykonywania swojego zawodu. No i ... złote ręce. Taki masażysta to skarb!"`,
     name: "Joanna Trzepiecińska",
     job: "aktorka",
-    text: `"W każdym zawodzie istnieją rzemieślnicy i artyści. Ci pierwsi nas zadowalają, ci drudzy wywołują zachwyt. Pan Wojtek Dyjak jest artystą! Ma talent, wiedzę i pasję do wykonywania swojego zawodu. No i ... złote ręce. Taki masażysta to skarb!"`,
     number: "3/7",
 },
 {
+    text: `"Pan Wojciech Dyjak jest bardzo dobrym masażystą. Z ogromnym znawstwem i wyczuciem podchodzi do pacjenta. Po jego masażach czuję się rozciągnięta i zdrowsza. Gorąco polecam."`,
     name: "Maria Piotrowska",
     job: "aktorka",
-    text: `"Pan Wojciech Dyjak jest bardzo dobrym masażystą. Z ogromnym znawstwem i wyczuciem podchodzi do pacjenta. Po jego masażach czuję się rozciągnięta i zdrowsza. Gorąco polecam."`,
     number: "4/7",
 },
 {
+    
+    text: `"Z Wojtkiem Dyjakiem współpracuję już od kilku lat. To najlepszy dowód na to, że jestem bardzo zadowolony z jego zabiegów. Jako osoba prowadząca aktywny tryb życia, potrzebuję sesji zapobiegających powstawaniu kontuzji jak i prawidłowej regenereacji organizmu. Wojtek dzięki swoim wybitnym umiejętnościom i profesjonalnej wiedzy jest w stanie mi taką opiekę zapewnić. Ponadto Wojtek jest kulturalnym, taktownym i dyskretnym masażystą, co sprawia, że spotkania z nim są niezbędną i miłą częścią tygodniowego grafiku. Gorąco polecam!"`,
     name: "Michał Englert",
     job: "operator filmowy, scenarzysta",
-    text: `"Z Wojtkiem Dyjakiem współpracuję już od kilku lat. To najlepszy dowód na to, że jestem bardzo zadowolony z jego zabiegów. Jako osoba prowadząca aktywny tryb życia, potrzebuję sesji zapobiegających powstawaniu kontuzji jak i prawidłowej regenereacji organizmu. Wojtek dzięki swoim wybitnym umiejętnościom i profesjonalnej wiedzy jest w stanie mi taką opiekę zapewnić. Ponadto Wojtek jest kulturalnym, taktownym i dyskretnym masażystą, co sprawia, że spotkania z nim są niezbędną i miłą częścią tygodniowego grafiku. Gorąco polecam!"`,
     number: "5/7",
 },
 {
+    text: `"Pan Wojciech Dyjak jest doskonałym specjalistą i potrafi pomóc w każdych problemach związanych z kręgosłupem. Regularne korzystanie z jego usług pozwala mi na zachowanie dobrej kondycji kręgosłupa. Polecam!!!"`,
     name: "Izabela Walczewska-Schneyder",
     job: "professional",
-    text: `"Pan Wojciech Dyjak jest doskonałym specjalistą i potrafi pomóc w każdych problemach związanych z kręgosłupem. Regularne korzystanie z jego usług pozwala mi na zachowanie dobrej kondycji kręgosłupa. Polecam!!!"`,
     number: "6/7",
 },
 {
+    text: `"Niezwykle cenię sobie masaże Wojtka Dyjaka. Jego profesjonalizm, znawstwo tematu, indywidualne podejście i otwartość. Perfekcyjnie dobiera rodzaj masażu do kondycji i potrzeb pacjenta. Od masażu leczniczego, rehabilitacyjnego, poprzez drenaż i masaż relaksacyjny. Jest również bardzo pomocny przy rehabilitacji wszelkich kontuzji, napięć mięśniowych. Do tego jest przesympatyczną, dyskretną, serdeczną osobą. Serdecznie polecam!"`,
     name: "Maja Ostaszewska",
     job: "aktorka",
-    text: `"Niezwykle cenię sobie masaże Wojtka Dyjaka. Jego profesjonalizm, znawstwo tematu, indywidualne podejście i otwartość. Perfekcyjnie dobiera rodzaj masażu do kondycji i potrzeb pacjenta. Od masażu leczniczego, rehabilitacyjnego, poprzez drenaż i masaż relaksacyjny. Jest również bardzo pomocny przy rehabilitacji wszelkich kontuzji, napięć mięśniowych. Do tego jest przesympatyczną, dyskretną, serdeczną osobą. Serdecznie polecam!"`,
     number: "7/7",
 },
 ];
